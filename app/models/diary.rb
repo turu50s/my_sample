e12852ad3c88d0c86de0092678d2dae792f6921e
tsuruki <sigeru0216@gmail.com>
2018-10-16 08:37:00 +0900

    Add original validation

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,17 @@
 class Diary < ApplicationRecord
-    validates :title, presense: true
-    validates :body, prsense: true
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true, length: { maximum: 10_000 }
+  validate :proper_title_and_body
+
+  private
+
+  def proper_title_and_body
+    unless title.starts_with?('今日')
+      errors.add(:title, 'は「今日」から始めて下さい。')
+    end
+
+    unless body.ends_with?('。')
+      errors.add(:body, 'は「。」で終了して下さい。')
+    end
+  end
 end
